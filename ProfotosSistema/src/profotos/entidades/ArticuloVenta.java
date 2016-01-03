7481fb7e3b5c6aef52414cd97d6e4bc1c4a03f6b
Jesus <mendozaalcantar@gmail.com>
2016-01-03 15:17:34 -0700

    2 commit

---
+++ ProfotosSistema/src/profotos/entidades/ArticuloVenta.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package profotos.entidades;
+
+/**
+ *
+ * @author Chuy
+ */
+class ArticuloVenta {
+    private int id;
+    private Double precio;
+    private int cantidad;
+    private Double costo;
+    private Double iva;
+    private Historial Historial;
+    private Articulo articulo;
+    
+   //<editor-fold defaultstate="collapsed" desc=" Getters and Setters del articuloVenta ">
+  
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the precio
+     */
+    public Double getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    /**
+     * @return the cantidad
+     */
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    /**
+     * @param cantidad the cantidad to set
+     */
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    /**
+     * @return the costo
+     */
+    public Double getCosto() {
+        return costo;
+    }
+
+    /**
+     * @param costo the costo to set
+     */
+    public void setCosto(Double costo) {
+        this.costo = costo;
+    }
+
+    /**
+     * @return the Historial
+     */
+    public Historial getHistorial() {
+        return Historial;
+    }
+
+    /**
+     * @param Historial the Historial to set
+     */
+    public void setHistorial(Historial Historial) {
+        this.Historial = Historial;
+    }
+
+    /**
+     * @return the iva
+     */
+    public Double getIva() {
+        return iva;
+    }
+
+    /**
+     * @param iva the iva to set
+     */
+    public void setIva(Double iva) {
+        this.iva = iva;
+    }
+
+    /**
+     * @return the articulo
+     */
+    public Articulo getArticulo() {
+        return articulo;
+    }
+
+    /**
+     * @param articulo the articulo to set
+     */
+    public void setArticulo(Articulo articulo) {
+        this.articulo = articulo;
+    }
+     //</editor-fold>
+}
---
+++ ProfotosSistema/src/profotos/entidades/TipoEmpleado.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package profotos.entidades;
+
+/**
+ *
+ * @author Chuy
+ */
+enum TipoEmpleado {
+ LIDER,FOTOGRAFO,VENDEDOR;   
+}
