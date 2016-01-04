0a8f5f5d4332be9c71eed8bef8537865e556f9e4
Jesus <mendozaalcantar@gmail.com>
2016-01-04 00:26:06 -0700

    Ventanas

---
+++ ProfotosSistema/src/profotos/entidades/clases/Egreso.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package profotos.entidades.clases;
+
+/**
+ *
+ * @author Chuy
+ */
+class Egreso {
+    private int id;
+    private String nombre;
+
+    
+    //<editor-fold defaultstate="collapsed" desc=" Getters and Setters del egreso ">
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    //</editor-fold>
+}
